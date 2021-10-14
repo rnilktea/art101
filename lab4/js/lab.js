9ce8856256785d0159d011bfc8b46ca58f42405c
rnilktea <lokinah.khan@gmail.com>
2021-10-13 21:26:49 -0700

    Lab 4

---
+++ lab4/js/lab.js
@@ -0,0 +1,27 @@
+//Pseudocode for Rock Paper Scissors.
+//Code for Rock Paper Scissors.
+//@link
+//@file
+//@author VincentWenzler LokinahKhan
+//@since
+
+//Pseudocode a computer task: Rock, Paper, Scissors
+    //Display action Choices
+      //Display Rock
+        //Display Paper
+          //Display Scissors
+    //Accept user input
+      //Input of Rock
+        //Input 1
+      //Input of Paper
+        //Input 2
+      //Input of Scissors
+        //Input 3
+    //Validate user move
+      //Record input number
+    //Randomize computer choice
+      //Generate whole number from 1-3
+    //Test for win or draw condition
+      //check if user number is same as computer number
+        //check if user number is different from computer number
+          //check which number computer has
