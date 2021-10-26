a5106c79619fa0b5dcaadc65be31b412fb5cad52
rnilktea <lokinah.khan@gmail.com>
2021-10-25 18:23:43 -0700

    Lab 7

--- lab7/js/lab.js
+++ lab7/js/lab.js
@@ -1,5 +1,28 @@
 /*
- * Author: Lokinah Khan
- * Created: 10.20.2021
+ * Author: Lokinah Khan & Sophia Syed
+ * Created: 10.25.2021
  * License: Public Domain
  */
+
+//a function that takes user input and sorts the letters
+function sortUserName() {
+
+  var userName = window.prompt("What is your name?: ");
+  console.log("userName =" , userName);
+
+  //split string to Array
+  var nameArray = userName.split('');
+
+  //sort the Array
+  var nameArraySort = nameArray.sort();
+  console.log("nameArraySort =" , nameArraySort);
+
+  //join array back to a string
+  var nameSorted = nameArraySort.join('');
+  console.log("nameSorted =" , nameSorted);
+
+  return nameSorted;
+}
+
+  //Output
+  document.writeln("Here is your new name: " , sortUserName() , "</br>");
