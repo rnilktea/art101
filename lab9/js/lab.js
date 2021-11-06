d346df00a73829fead7c970c3f71c274dc526671
rnilktea <lokinah.khan@gmail.com>
2021-11-06 16:26:07 -0700

    lab 9

--- lab9/js/lab.js
+++ lab9/js/lab.js
@@ -4,4 +4,29 @@
  * License: Public Domain
  */
 
- 
+//function
+window.onload = function () {
+
+  //Use getElementById() to find your output <div> and assign it to a variable outputEl
+  var outputEl = document.getElementById("output");
+
+  //Create a new element with document.createElement("p") and assign it to a variable new1El
+  var new1El = document.createElement("p");
+
+  //Change the html attribute of new1El to say something new.
+  new1El.innerHTML = "Something new.";
+  new1El.id = "new-one";
+
+  //Create another new element and assign it to a variable new2El
+  var new2El = document.createElement("p");
+  new1El.id = "new-two";
+
+  //Change the html attribute of new2El to say something else.
+  new2El.innerHTML = "Something else.";
+
+  //Append both new elements one at a time using appendChild() to your output <div>
+  outputEl.appendChild(new1El);
+  outputEl.appendChild(new2El);
+
+  outputEl.className = "changed";
+}
