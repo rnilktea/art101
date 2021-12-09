b9e1417db9fb9462170afceec5d4aeb7601b7a0d
rnilktea <lokinah.khan@gmail.com>
2021-12-09 15:58:09 -0800

    attempted lab 15

--- lab15/js/lab.js
+++ lab15/js/lab.js
@@ -3,3 +3,27 @@
  * Created: 12.08.2021
  * License: Public Domain
  */
+
+ function randomQuote() {
+   $.ajax({
+     // The URL for the request (from the api docs)
+     url: "https://api.quotable.io/random",
+     // Whether this is a POST or GET request
+     type: "GET",
+     dataType: "json",
+     // What do we do when the api call is successful
+     //all the action goes in here
+   })
+   .done(function(data) {
+     var author = data.author;
+     var quote = data.content;
+     console.log(data);
+     $("#quote").html("<h4>"+quote+"</h4><br>- "+author);
+   })
+   .fail(function(request, error) {
+     console.log(request, error);
+   })
+ }
+
+ // when button is clicked, calls randomQuote()
+ $("#activate").click(randomQuote);
